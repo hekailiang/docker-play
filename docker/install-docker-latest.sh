d4b77eee16acddd649cd024abe01a08a06476f9b
hekailiang <hekailiang@gmail.com>
2015-02-15 15:38:59 +0800

    add install latest docker script

---
+++ docker/install-docker-latest.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+sudo apt-get update
+sudo apt-get -y install linux-image-extra-$(uname -r)
+sudo sh -c "wget -qO- https://get.docker.io/gpg | apt-key add -"
+sudo sh -c "echo deb http://get.docker.io/ubuntu docker main\ > /etc/apt/sources.list.d/docker.list"
+sudo apt-get update
+sudo apt-get -y install lxc-docker
