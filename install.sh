5b679436934b01792230865af2dc1cfa41880c6c
Henry.He <hekailiang@gmail.com>
2021-05-11 20:29:07 -0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -138,5 +138,4 @@ info "launching ${artifactId} application ..."
 info "install mariadb database at ${artifactId}/.database"
 JAVA_OPTS="-server -Xms1800m -Xmx1800m -Xmn680m -XX:MetaspaceSize=340m -XX:MaxMetaspaceSize=340m -XX:HeapDumpPath=$PWD/logs "
 JAVA_OPTS="-agentlib:jdwp=transport=dt_socket,server=y,suspend=n,address=8000 $JAVA_OPTS"
-cd "${artifactId}" 
-java "$JAVA_OPTS"  -jar app/"${artifactId}"-simulator/target/"${artifactId}"-simulator-1.0.0-executable.jar
+cd "${artifactId}" && java "$JAVA_OPTS"  -jar app/"${artifactId}"-simulator/target/"${artifactId}"-simulator-1.0.0-executable.jar
